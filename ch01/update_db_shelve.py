9537a8a76aba3237c7520c37ef1d9de35dcef53f
WD41 <damar.wicaksono@psi.ch>
2014-09-02 14:31:16 +0200

    Add Shelve-based database example
    Shelve stores the Python object similar to Pickle with an addition
    to key-based access to store record.

---
+++ ch01/update_db_shelve.py
@@ -0,0 +1,10 @@
+# update_db_shelve.py
+from initdata import tom
+import shelve
+
+db = shelve.open("people-shelve")
+sue = db["sue"]
+sue["pay"] *= 1.50
+db["sue"] = sue
+db["tom"] = tom
+db.close()
